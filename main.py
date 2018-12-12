f3e2bcae7fa3ac8ae87d4ef79f2ab17f5586a89d
FSE IT <bogo@SCI-7056.local>
2018-12-12 14:49:58 +1100

    Pushed first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Create function called greetings()
+# First parameter: first name
+# Second parameter: last name
+# Last parameter: age
+
+# greetings('Samir', 'Chahine', 21)
+# "Hello Samir Chahine, you are 21 years old"def fname(arg):
+
+
+def greetings(first_name):
+    print("Hello " + first_name)
+
+first_names = ['john', 'sam', 'daniel', 'rachel']
+
+for index in range(0, 4):
+    greetings(first_names[index])
